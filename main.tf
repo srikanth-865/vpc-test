672ef9ddf8bfbfb5ce507eba40231dce34adc8a3
unknown <srikanthram87@gmail.com>
2026-07-29 09:12:13 +0530

    oil&karam

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 module "vpc" {
     #source = "../terraform-aws-vpc"
-    source = git::https://github.com/srikanth-865/terraform-aws-vpc.git?ref=main
+    source = "git::https://github.com/srikanth-865/terraform-aws-vpc.git?ref=main"
     project =  "Roboshop"
     environment =  "Dev"
     is_peering_require = false
